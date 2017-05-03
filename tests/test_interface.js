ea7dcf1797ffa886da41ba0f819d094de575cbff
kidlovess <stdsitsuwan2008@gmail.com>
2017-05-03 11:14:21 +0700

    update dragdrop all menu component
    update dragdrop all menu component

--- tests/test_interface.js
+++ tests/test_interface.js
@@ -3,10 +3,10 @@ var async = require('async');
 module.exports = {
 	beforeEach: function (browser) {
 		var loginPage = browser.page.opsportal.login(),
-			menuSection = browser.page.opsportal.menu();
+		menuSection = browser.page.opsportal.menu();
 
 		browser
-			.maximizeWindow()
+		.maximizeWindow()
 			.url(browser.launchUrl) // Go to http://localhost:1337/page/opsportal
 			.waitForElementVisible('body', 1000); // Wait for the login page
 
@@ -75,40 +75,237 @@ module.exports = {
 					console.log("lIndex:"+lastNewPageIndex);
 					
 				});
-						
+
+
+				appInterfacePage
+				.selectInterfaceTab()
+				.selectInterface(0);
+
+				next();
+			},
+			function(next){
+				browser.pause(2000);
+				browser.moveToElement('div[view_id="ab-component-space"]', 5, 17)
+				.mouseButtonDown(0)
+					.moveToElement('div[view_id="ab-interface-componentList"]',  5,  5) // Move to offset position of 200(x) 600(y)
+					.mouseButtonUp(0)
+					.pause(2000).end();
+
+					next();
+				},
+				function(next){
+					//var appInterfacePage = browser.page.app_builder.interface_list();
+					
+				}
+				], function (err) {
+					browser.pause(500).end();
+				});
+	},
+	'Select Grid components' : function (browser) {
+		var appListPage = browser.page.app_builder.app_list();
+		var appInterfacePage = browser.page.app_builder.interface_list();
+		var lastNewPageIndex,lastAppIndex;
+
+		async.series([
+			// Get last item index
+			function (next) {
+				browser.elements('css selector', appListPage.elements.appItem.selector, function (elems) {
+					lastAppIndex = elems.value.length;
+					appListPage.selectApp(lastAppIndex);
+
+					browser.elements('css selector', 'div[view_id="ab-interface-tree"] .ab-page-list-item', function (elems) {
+						lastNewPageIndex = elems.value.length;
+
+						console.log("lIndex:"+lastNewPageIndex);
+
+					});
 
 					appInterfacePage
 					.selectInterfaceTab()
 					.selectInterface(0);
 
 					next();
+				});
 			},
 			function(next){
 				browser.pause(2000);
-			browser.moveToElement('div[view_id="ab-component-space"]', 5, 15)
-					.mouseButtonDown(0)
+				browser.moveToElement('div[view_id="ab-component-space"]', 5, 51)
+				.mouseButtonDown(0)
 					.moveToElement('div[view_id="ab-interface-componentList"]',  5,  5) // Move to offset position of 200(x) 600(y)
-	      			.mouseButtonUp(0);
+					.mouseButtonUp(0)
+					.pause(2000).end();
 
-	      			next();
-			},
-			//select menu component
-			function(next){
-				browser.pause(2000);
-				browser.moveToElement('div[view_id="ab-component-space"]', 5, 45)
-					.mouseButtonDown(0)
-					.moveToElement('div[view_id="ab-interface-componentList"]',  5,  5) // Move to offset position of 200(x) 600(y)
-	      			.mouseButtonUp(0);
-
-	      			next();
-			},
-			function(next){
-					//var appInterfacePage = browser.page.app_builder.interface_list();
+				next();
 					
 			}
-			], function (err) {
-			browser.pause(500).end();
-		});
+		], function (err) {
+				browser.pause(500).end();
+		});		
+
 	},
+	'Select Form components' : function (browser) {
+		var appListPage = browser.page.app_builder.app_list();
+		var appInterfacePage = browser.page.app_builder.interface_list();
+		var lastNewPageIndex,lastAppIndex;
+
+		async.series([
+			// Get last item index
+			function (next) {
+				browser.elements('css selector', appListPage.elements.appItem.selector, function (elems) {
+					lastAppIndex = elems.value.length;
+					appListPage.selectApp(lastAppIndex);
+
+					browser.elements('css selector', 'div[view_id="ab-interface-tree"] .ab-page-list-item', function (elems) {
+						lastNewPageIndex = elems.value.length;
+
+						console.log("lIndex:"+lastNewPageIndex);
+
+					});
+
+					appInterfacePage
+					.selectInterfaceTab()
+					.selectInterface(0);
+
+					next();
+				});
+			},
+			function(next){
+				browser.pause(2000);
+				browser.moveToElement('div[view_id="ab-component-space"]', 5, 85)
+				.mouseButtonDown(0)
+					.moveToElement('div[view_id="ab-interface-componentList"]',  5,  5) // Move to offset position of 200(x) 600(y)
+					.mouseButtonUp(0)
+					.pause(2000).end();
+					
+			}
+		], function (err) {
+				browser.pause(500).end();
+		});		
+
+
+	},
+	'Select View components' : function (browser) {
+var appListPage = browser.page.app_builder.app_list();
+		var appInterfacePage = browser.page.app_builder.interface_list();
+		var lastNewPageIndex,lastAppIndex;
+
+		async.series([
+			// Get last item index
+			function (next) {
+				browser.elements('css selector', appListPage.elements.appItem.selector, function (elems) {
+					lastAppIndex = elems.value.length;
+					appListPage.selectApp(lastAppIndex);
+
+					browser.elements('css selector', 'div[view_id="ab-interface-tree"] .ab-page-list-item', function (elems) {
+						lastNewPageIndex = elems.value.length;
+
+						console.log("lIndex:"+lastNewPageIndex);
+
+					});
+
+					appInterfacePage
+					.selectInterfaceTab()
+					.selectInterface(0);
+
+					next();
+				});
+			},
+			function(next){
+				browser.pause(2000);
+				browser.moveToElement('div[view_id="ab-component-space"]', 5, 119)
+				.mouseButtonDown(0)
+					.moveToElement('div[view_id="ab-interface-componentList"]',  5,  5) // Move to offset position of 200(x) 600(y)
+					.mouseButtonUp(0)
+					.pause(2000).end();
+					
+			}
+		], function (err) {
+				browser.pause(500).end();
+		});		
+
+
+	},
+	'Select Link components' : function (browser) {
+var appListPage = browser.page.app_builder.app_list();
+		var appInterfacePage = browser.page.app_builder.interface_list();
+		var lastNewPageIndex,lastAppIndex;
+
+		async.series([
+			// Get last item index
+			function (next) {
+				browser.elements('css selector', appListPage.elements.appItem.selector, function (elems) {
+					lastAppIndex = elems.value.length;
+					appListPage.selectApp(lastAppIndex);
+
+					browser.elements('css selector', 'div[view_id="ab-interface-tree"] .ab-page-list-item', function (elems) {
+						lastNewPageIndex = elems.value.length;
+
+						console.log("lIndex:"+lastNewPageIndex);
+
+					});
+
+					appInterfacePage
+					.selectInterfaceTab()
+					.selectInterface(0);
+
+					next();
+				});
+			},
+			function(next){
+				browser.pause(2000);
+				browser.moveToElement('div[view_id="ab-component-space"]', 5, 153)
+				.mouseButtonDown(0)
+					.moveToElement('div[view_id="ab-interface-componentList"]',  5,  5) // Move to offset position of 200(x) 600(y)
+					.mouseButtonUp(0)
+					.pause(2000).end();
+					
+			}
+		], function (err) {
+				browser.pause(500).end();
+		});		
+
+
+	},
+	'Select Tab components' : function (browser) {
+
+var appListPage = browser.page.app_builder.app_list();
+		var appInterfacePage = browser.page.app_builder.interface_list();
+		var lastNewPageIndex,lastAppIndex;
+
+		async.series([
+			// Get last item index
+			function (next) {
+				browser.elements('css selector', appListPage.elements.appItem.selector, function (elems) {
+					lastAppIndex = elems.value.length;
+					appListPage.selectApp(lastAppIndex);
+
+					browser.elements('css selector', 'div[view_id="ab-interface-tree"] .ab-page-list-item', function (elems) {
+						lastNewPageIndex = elems.value.length;
+
+						console.log("lIndex:"+lastNewPageIndex);
+
+					});
+
+					appInterfacePage
+					.selectInterfaceTab()
+					.selectInterface(0);
+
+					next();
+				});
+			},
+			function(next){
+				browser.pause(2000);
+				browser.moveToElement('div[view_id="ab-component-space"]', 5, 187)
+				.mouseButtonDown(0)
+					.moveToElement('div[view_id="ab-interface-componentList"]',  5,  5) // Move to offset position of 200(x) 600(y)
+					.mouseButtonUp(0)
+					.pause(2000).end();
+					
+			}
+		], function (err) {
+				browser.pause(500).end();
+		});		
+
+	}
 
 };
