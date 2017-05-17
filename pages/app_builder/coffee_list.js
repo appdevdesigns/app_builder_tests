32852f63a664f4d9b59dcdebda71bfafc85a2e83
kidlovess <stdsitsuwan2008@gmail.com>
2017-05-17 17:32:32 +0700

    update coffee list

--- pages/app_builder/coffee_list.js
+++ pages/app_builder/coffee_list.js
@@ -1,22 +1,23 @@
-equire('../../shared_libs/global_config.js');
+require('../../shared_libs/global_config.js');
 
 module.exports = {
-	url: 'http://localhost:1337/page/opsportal',
+	url: 'http://139.59.109.30:1337/page/opsportal',
 	elements: {
 			menuItem: 'span[app-label-key="opp.menu"]',
 			coffeeShopItem: 'li[area="ab-app-coffee-shop"]'
-		},
+	},
 	commands: [{
 		selectCoffeeShop: function () {
-			this
-				.waitForElementVisible('@menuItem', 99999)
+			this.waitForElementVisible('@menuItem', 99999)
 				.click('@menuItem')
-				.waitForElementVisible('@appBuilderItem', 500)
-				.click('@appBuilderItem')
+				.waitForElementVisible('@coffeeShopItem', 500)
+				.click('@coffeeShopItem')
+				.waitForElementVisible('@menuItem', 3000)
 				.click('@menuItem');
 
+
 			return this;
-		}
+		},
 
 
 	}]
